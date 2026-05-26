8843b888b962f14139bb6d913b8025eb3a514751
konfuzz <konfuzz@gmail.com>
2026-05-26 09:29:38 +0400

    feat: add visual editor page

--- src/config.ts
+++ src/config.ts
@@ -19,6 +19,7 @@ export const SOCIAL_LINKS = {
 
 // Navigation Links
 export const NAV_LINKS = [
+  { href: '/vidual-editor/', label: 'Vidual Editor' },
   { href: '/#features', label: 'Features' },
   { href: '/#testimonials', label: 'Testimonials' },
   { href: '/pricing/', label: 'Pricing' },
